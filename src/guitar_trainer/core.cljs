11568956bdd58fc1364918327e2d281a330fb929
Nils Blum-Oeste <nils@blum-oeste.de>
2019-06-23 12:44:04 +0200

    Add hacky pitch detection

--- src/guitar_trainer/core.cljs
+++ src/guitar_trainer/core.cljs
@@ -6,24 +6,36 @@
 ;; define your app data so that it doesn't get over-written on reload
 (defonce !app-state (atom {:input-devices '()
                            :current-input nil
-                           :audio-context nil}))
+                           :audio-context nil
+                           :analyser nil
+                           :current-pitch "--"}))
 
 (defn set-current-input [device-id]
   (swap! !app-state assoc :current-input device-id))
 
+(defn set-input-devices [input-devices]
+  (swap! !app-state assoc :input-devices input-devices))
+
+(defn set-current-pitch [note frequency]
+  (swap! !app-state assoc :current-pitch {:note note
+                                          :frequency frequency}))
+
+(defn set-analyser [analyser]
+  (swap! !app-state assoc :analyser analyser))
+
 (defn current-input []
   (:current-input @!app-state))
 
-(defn set-input-devices [input-devices]
-  (swap! !app-state assoc :input-devices input-devices))
+(defn current-audio-context []
+  (:audio-context @!app-state))
+
+(defn current-analyser []
+  (:analyser @!app-state))
 
 (defn create-audio-context []
   (let [constructor (or js/window.AudioContext
                         js/window.webkitAudioContext)]
     (constructor.)))
-
-(defn current-audio-context []
-  (:audio-context @!app-state))
 
 (defn get-audio-context []
   (swap! !app-state assoc :audio-context (create-audio-context)))
@@ -45,11 +57,11 @@
                  input-devices)))))
 
 (defn get-audio-input+ [device-id]
-  (-> (js/navigator.mediaDevices.getUserMedia (clj->js {:audio {"deviceId" device-id}
-                                                        :video false}))))
+  (-> (js/navigator.mediaDevices.getUserMedia (clj->js {:audio {"deviceId" device-id}}))))
 
 (defn process-audio
   [audio-processing-event]
+  (println ">>> processing audio event")
   (let [input-buffer (aget audio-processing-event "inputBuffer")
         output-buffer (aget audio-processing-event "outputBuffer")
         ;; FIXME converting the TypedArray like this might be too expensive
@@ -62,14 +74,6 @@
               (count input-data))
            :timestamp (.getTime (js/Date.)))))
 
-(defn init! []
-  (get-audio-context)
-  (-> (get-input-devices+)
-      (.then #(set-current-input (first %)))))
-
-(defn get-app-element []
-  (gdom/getElement "app"))
-
 (defn input-selector-ui
   [{:keys [current-input input-devices]}]
   [:select {:value current-input
@@ -78,34 +82,117 @@
      ^{:key device-id}
      [:option {:value device-id} label])])
 
+(defn find-fundamental-freq [buffer sample-rate]
+  (let [n 1024                                        ;; buffer size / 2
+        best-r (atom 0)
+        best-k (atom -1)
+        k-start 8                                     ;; frame start distance
+        k-end 1000
+        r-threshold 0.9                               ;; threshold for early exit
+        r-success 0.0025                              ;; threshold for success case
+        normalise #(/ (- % 128) 128)
+        continue (atom true)
+        buffer (js->clj (js/Array.from buffer))]
+    (doseq [k (range k-start (inc k-end))
+            :while @continue]
+      (let [first-frame (take n buffer)
+            second-frame (drop k buffer)
+            frame-pairs (map (fn [b1 b2] [b1 b2])
+                             first-frame
+                             second-frame)
+            sum (reduce (fn [sum [b1 b2]]
+                          (+ sum
+                             (* (normalise b1)
+                                (normalise b2))))
+                        0
+                        frame-pairs)
+            r (/ sum (+ n k))]
+        #_(println ">>> r: " r
+                   "... " sum
+                   ">>> frames "
+                   (first first-frame)
+                   (first second-frame))
+        (when (> r @best-r)
+          (reset! best-r r)
+          (reset! best-k k))
+        (when (> r r-threshold)
+          (println ">>> found above threshold")
+          (reset! continue false))))
+    (println ">>> best-r" @best-r)
+    (when (> @best-r r-success)
+      (/ sample-rate @best-k))))
+
+(defn find-closest-note [fundamental-freq]
+  {:note "note names not yet implemented"})
+
+(defn detect-pitch []
+  (let [audio-context (current-audio-context)
+        analyser (current-analyser)
+        buffer (js/Uint8Array. (.-fftSize analyser))
+        _ (.getByteTimeDomainData analyser buffer)
+        fundamental-freq (find-fundamental-freq buffer (.-sampleRate audio-context))]
+    (if fundamental-freq
+      (let [{:keys [note frequency]} (find-closest-note fundamental-freq)
+            ;; TODO add distance from target here
+            ]
+        (set-current-pitch note fundamental-freq))
+      (do
+        (set-current-pitch nil fundamental-freq)))
+    (swap! !app-state assoc
+           :timestamp (.getTime (js/Date.)))
+    (js/window.requestAnimationFrame detect-pitch)))
+
 (defn reset-audio-processing!
   [_]
   ;; TODO close exisiting resources
+  (when-not (current-audio-context)
+    (get-audio-context))
+  (println ">>> resetting audio processing")
   (-> (get-audio-input+ (current-input))
       (.then (fn [stream]
+               (println ">>> creating processing pipeline")
                (let [context (current-audio-context)
                      source (.createMediaStreamSource context stream)
-                     processor (.createScriptProcessor context 4096 1 1)]
-                 (.connect source processor)
-                 (.connect processor (.-destination context))
-                 (set! (.-onaudioprocess processor)
-                       process-audio)))))
-  )
+                     analyser (.createAnalyser context)
+                     ;processor (.createScriptProcessor context 4096 1 1)
+                     ]
+                 (set-analyser analyser)
+                 (println ">>> setting audio node properties")
+                 (set! (.-fftSize analyser) 2048)
+                 ;(set! (.-onaudioprocess processor) process-audio)
+                 (println ">>> connecting audio nodes")
+                 (.connect source analyser)
+                 ;(.connect analyser (.-destination context))
+                 (detect-pitch))))))
 
 (defn start-ui []
   [:button {:on-click reset-audio-processing!}
    "Start"])
 
+(defn note-ui [{:keys [note frequency]}]
+  (let [default-message "not detected"]
+    [:div
+     [:h2 "Current note: " (or note default-message)]
+     [:h2 "Current frequency: " (or frequency default-message)]]))
+
 (defn main-ui []
-  (let [{:keys [] :as app-state} @!app-state]
+  (let [{:keys [current-pitch] :as app-state} @!app-state]
     [:div
      [:h1 "Guitar Trainer"]
      [input-selector-ui app-state]
      [start-ui]
+     [note-ui current-pitch]
      [:p (str app-state)]]))
 
 (defn mount [el]
   (reagent/render-component [main-ui] el))
+
+(defn get-app-element []
+  (gdom/getElement "app"))
+
+(defn init! []
+  (-> (get-input-devices+)
+      (.then #(set-current-input (first %)))))
 
 (defn mount-app-element []
   (when-let [el (get-app-element)]
@@ -122,4 +209,4 @@
   ;; optionally touch your app-state to force rerendering depending on
   ;; your application
   ;; (swap! app-state update-in [:__figwheel_counter] inc)
-)
+  )
